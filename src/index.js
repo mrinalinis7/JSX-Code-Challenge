3e4e6ee4cfb692e751168efa39a69070cb312c9f
Mrinalini <tina.sd7@gmail.com>
2025-02-02 15:24:07 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <li>Mango</li>
+    <li>Salad</li>
+    <li>Berries</li>
+  </div>,
+  document.getElementById("root")
+);
